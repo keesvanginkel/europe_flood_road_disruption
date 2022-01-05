fe218f7f31302e03c42ebed7257e9f961d08b12c
Kees van Ginkel <kees.vanginkel@deltares.nl>
2022-01-05 16:43:31 +0100

    Added select function to aggregation functionality

--- scripts/run_aggregation.py
+++ scripts/run_aggregation.py
@@ -3,22 +3,9 @@
 import visualisations_new as vis
 from utils import load_config
 
-config = load_config(file='config_agg_unc.json')
+config = load_config(file='config.json')
 
-#ignore = ['austria_buffer', 'belgium_0cm', 'belgium_50cm', 'belgium_distance', 'belgium_fla',  #'belgium_giant_component',
-#          'belgium_n3_alllevels', 'belgium_n3_pri', 'belgium_n3_prisec', 'belgium_shifted_centroids', 'belgium_wal',
-#          'benelux', 'latvia_giant_component', 'poland_2000', 'rhine_alpine_corridor']
 
-#ignore = ['albania', 'austria', 'belgium', 'bulgaria', 'croatia', 'czechia', 'denmark', 'estonia', 'figures',
-#           'finland', 'france', 'germany', 'greece', 'hungary', 'ireland', 'italy',  'latvia_old', #latvia
-#           'lithuania', 'macedonia', 'netherlands', 'norway', 'poland', 'portugal', 'romania', 'serbia',
-#           'slovakia', 'slovenia', 'spain', 'sweden', 'switzerland', 'united kingdom']
-
-ignore= ['austria_buffer', 'belgium_0cm', 'belgium_50cm',  'belgium_distance', 'belgium_fla', 'belgium_buffer',
-         'belgium_giant_component_part1', 'belgium_giant_component_part2', #'belgium_giant_component_all',
-         'belgium_n3_alllevels', 'belgium_n3_pri', 'belgium_n3_prisec', 'belgium_shifted_centroids',
-         'belgium_wal', 'benelux', 'germany', 'latvia_giant_component', 'poland_2000', 'rhine_alpine_corridor']
-
-#vis.aggregate_results_step1(ignore=ignore,config=config) #to .csv per # combinations
-vis.aggregate_results_step2(ignore=ignore,config=config)
+#vis.aggregate_results_step1(select=['hungary'],config=config) #to .csv per # combinations
+vis.aggregate_results_step2(select=['hungary'],config=config)
 
